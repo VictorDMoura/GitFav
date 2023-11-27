d5575e651db15e99858b425aeaf1d20a54fa5352
VictorDMoura <mouramvictor@gmail.com>
2023-11-27 19:57:20 -0300

    feat: Add create row

--- src/Favorites.js
+++ src/Favorites.js
@@ -2,7 +2,21 @@
 export class Favorites{
     constructor(root){
         this.root = document.querySelector(root)
+        this.load()
     }
+
+    load(){
+        this.entries = [
+            {
+                login: "maykbrito",
+                name: "Mayk Brito",
+                public_repos: 123,
+                followers: 123
+            }
+        ]
+    }
+
+
 }
 
 
@@ -11,7 +25,61 @@ export class FavoritesView extends Favorites{
     constructor(root){
         super(root)
         this.tbody = document.querySelector('table.data tbody')
-        console.log(this.tbody)        
+        this.update()
+    }
+
+    update(){
+        this.removeAllTr()
+
+        this.entries.forEach((user) => {
+            const row = this.createRow()
+            
+            row.querySelector('.user img').src = `https://github.com/${user.login}.png`
+            row.querySelector('.user img').alt = `Imagem de ${user.name}`
+            row.querySelector('.user a').href =  `https://github.com/${user.login}`
+            row.querySelector('.user p').textContent = user.name
+            row.querySelector('.user span').textContent = user.login
+            row.querySelector('.repositories').textContent = user.public_repos
+            row.querySelector('.followers').textContent = user.followers
+            row.querySelector('.remove').onclick = () => {
+                const isOk = confirm('Tem Certeza que deseja deletar essa linha?')
+                if(isOk){
+                    this.delete(user)
+                    this.update()
+                }
+            }
+
+
+            this.tbody.append(row)
+        })
+    }
+
+    createRow(){
+        const tr = document.createElement('tr')
+        tr.innerHTML = `
+        <td class="user">
+              <img
+                src="https://github.com/maykbrito.png"
+                alt="Image do usuário maykbrito"
+              />
+              <a href="https://github.com/maykbrito">
+                <p>Mayk Brito</p>
+                <span>/maykbrito</span>
+              </a>
+            </td>
+            <td class="repositories">123</td>
+            <td class="followers">1234</td>
+            <td class="remove">Remover</td>
+        `
+        return tr
+    }
+
+
+    removeAllTr(){
+        this.tbody.querySelectorAll('tr')
+            .forEach((tr) => {
+                tr.remove()
+            })
     }
 }
 
